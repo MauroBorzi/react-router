7dc672cfc4e3af69b2f3ce9a2901431a08cf3cd6
MauroBorzi <mauroborzi91@gmail.com>
2025-09-05 15:26:09 +0200

    [add]projectStructure

---
+++ src/pages/AboutPage.jsx
@@ -0,0 +1,9 @@
+const AboutPage = () => {
+  return (
+    <div>
+      <h1>Aboutpage</h1>
+    </div>
+  )
+}
+
+export default AboutPage
---
+++ src/pages/ArticlePage.jsx
@@ -0,0 +1,9 @@
+const ArticlePage = () => {
+  return (
+    <div>
+      <h1>ArticlePage</h1>
+    </div>
+  )
+}
+
+export default ArticlePage
